5c861067b478c49af0b32987d96a0d978ec96768
ZabihNajeeb <zabih_najeebi@yahoo.com>
2024-07-02 21:05:49 -0400

    More upate connection of

--- storeSearch/storeSearch/SearchViewController.swift
+++ storeSearch/storeSearch/SearchViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SearchViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
 }
+
+// MARK: - Search Bar Delegate
+extension SearchViewController: UISearchBarDelegate {
+  func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+    print("The search text is: '\(searchBar.text!)'")
+  }
+}
+
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(
+      _ tableView: UITableView,
+      numberOfRowsInSection section: Int
+    ) -> Int {
+      return 0
+    }
+    
+    func tableView(
+      _ tableView: UITableView,
+      cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+      return UITableViewCell()
+    }
+  }
 
